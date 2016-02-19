559140a43f394052934a69070b91377b1687260d
Win8 <trot505@yandex.ru>
2016-02-19 15:18:33 +0400

    all

--- Fuel/MainWindow.xaml.cs
+++ Fuel/MainWindow.xaml.cs
@@ -1,28 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Office.Interop.Excel;
+using System.Linq;
+using System;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace Fuel
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
+        Company[] str;
         public MainWindow()
         {
             InitializeComponent();
+            string fileC = File.ReadAllText(@"company.json", UTF8Encoding.UTF8);
+
+            Company[] str = JsonConvert.DeserializeObject<Company[]>(fileC);
+
+            Company s = Array.Find(str, r => r.NameBash == "Регион 1");
+
+            textN.Text = s.Name;
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Файлы excel|*.xls;*.xlsx";
+            OPF.Title = "Выберите файл excel";
+            if (OPF.ShowDialog() != null)
+            {
+                fileName.Text = OPF.FileName;
+            }
+
+        }
+
+        
+        private void Jp_Click(object sender, RoutedEventArgs e)
+        {
+           
+         
+        }
+
+
+       
     }
 }
---
+++ Fuel/class/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuel {
+    [DataContract]
+    class Company
+    {
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+        [DataMember(Name = "NameBash")]
+        public string NameBash { get; set; }
+        [DataMember(Name = "NameLuk")]
+        public string NameLuk { get; set; }
+
+        public Company(string n, string nb, string nl)
+        {
+            Name = n;
+            NameBash = nb;
+            NameLuk = nl;
+        }
+
+    }
+}
