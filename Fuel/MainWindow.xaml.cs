f7c4d491f2402980f2567b56f81fff8b6a844229
Win8 <trot505@yandex.ru>
2016-02-20 15:56:12 +0400

    all

--- Fuel/MainWindow.xaml.cs
+++ Fuel/MainWindow.xaml.cs
@@ -6,12 +6,11 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections.Generic;
-using System.IO;
 using System.Runtime.Serialization.Json;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
-
+using System.Collections.ObjectModel;
 
 namespace Fuel
 {
@@ -20,17 +19,25 @@ namespace Fuel
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        Company[] str;
+        ObservableCollection<Company> company = new ObservableCollection<Company>();
+       //Company[] str;
         public MainWindow()
         {
             InitializeComponent();
             string fileC = File.ReadAllText(@"company.json", UTF8Encoding.UTF8);
-
             Company[] str = JsonConvert.DeserializeObject<Company[]>(fileC);
 
-            Company s = Array.Find(str, r => r.NameBash == "Регион 1");
+            foreach (Company s in str)
+            {
+                company.Add(s);
+            }
+            //str = JsonConvert.DeserializeObject<Company[]>(fileC);
 
-            textN.Text = s.Name;
+            //Company s = Array.Find(str, r => r.NameBash == "Регион 1");
+
+            //textN.Text = s.Name;
+
+            CompanyGrid.ItemsSource = company;
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
@@ -38,21 +45,84 @@ namespace Fuel
             OpenFileDialog OPF = new OpenFileDialog();
             OPF.Filter = "Файлы excel|*.xls;*.xlsx";
             OPF.Title = "Выберите файл excel";
-            if (OPF.ShowDialog() != null)
+            if (OPF.ShowDialog() == true)
             {
                 fileName.Text = OPF.FileName;
+                //var exelP = new Microsoft.Office.Interop.Excel.Application();
+                //var WorkBookExcel = exelP.Workbooks.Open(OPF.FileName);
+                //var WorkSheetExcel = (Microsoft.Office.Interop.Excel.Worksheet)WorkBookExcel.Sheets[1];
+                //object[,] arr = (object[,])WorkSheetExcel.UsedRange.Value;
+                //dataEx.ItemsSource = arr;
             }
-
+            
         }
 
         
-        private void Jp_Click(object sender, RoutedEventArgs e)
+     
+
+        private void add_Click(object sender, RoutedEventArgs e)
         {
-           
-         
+            addcompany tr = new addcompany();
+            if (tr.ShowDialog() == true)
+            {
+                
+                company.Add(new Company(tr.name.Text, tr.nameBash.Text, tr.nameLuk.Text));                
+                tr.Close();
+            }
+            
+
         }
 
+        private void deleteCompany()
+        {
+            if (CompanyGrid.SelectedIndex >= 0)
+            {
+                Company c = CompanyGrid.SelectedItem as Company;
+                company.Remove(c);
+            }
+        }
 
-       
+        private void updateCompany(string n, string nb, string nl)
+        {
+            if (CompanyGrid.SelectedIndex >= 0)
+            {
+                Company newC = new Company(n,nb,nl);
+                Company c = CompanyGrid.SelectedItem as Company;
+                company.Remove(c);
+                company.Add(newC);
+            }
+        }
+
+        private void CompanyGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            upDel f = new upDel();
+            if (f.ShowDialog() == true)
+            {
+                addcompany tr = new addcompany();
+                Company c = CompanyGrid.SelectedItem as Company;
+                tr.name.Text = c.Name;
+                tr.nameBash.Text = c.NameBash;
+                tr.nameLuk.Text = c.NameLuk;
+                if(tr.ShowDialog() == true)
+                {
+                    updateCompany(tr.name.Text, tr.nameBash.Text, tr.nameLuk.Text);
+                }
+                tr.Close();
+
+            } else {
+                deleteCompany();                    
+            }
+            CompanyGrid.SelectedIndex = -1;
+            f.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"company.json", false))
+            {
+                file.WriteLine(JsonConvert.SerializeObject(company));
+            }
+           
+        }
     }
 }
